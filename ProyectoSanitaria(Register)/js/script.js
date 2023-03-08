bc17ac1eb4318028f6473dd7df7506a619cce53e
carla <carlajilo@gmail.com>
2023-03-08 20:31:04 +0100

    validation

--- ProyectoSanitaria(Register)/js/script.js
+++ ProyectoSanitaria(Register)/js/script.js
@@ -1,40 +1,25 @@
-let inputName = document.getElementById("inputName")
-let inputLastNames = document.getElementById("inputLastNames")
-let inputEmail = document.getElementById("inputEmail")
-let inputCenter = document.getElementById("inputCenter")
-let inputCourse = document.getElementById("inputCourse")
-let inputPassword = document.getElementById("inputPassword")
-let inputRepeatPassword = document.getElementById("inputRepeatPassword")
-let btnsub = document.getElementById("btnsub")
-
-
-const processForm = (event) =>{
-    event.preventDefault();
+const processForm = () => {
     let newTecnico = {
-        nombre:inputName.value,
-        apellidos:inputLastNames.value,
-        email:inputEmail.value,
-        password:inputPassword.value,
-        curso:inputCourse.value,
-        centro:inputCenter.value,
+        nombre: inputName.value,
+        apellidos: inputLastNames.value,
+        email: inputEmail.value,
+        password: inputPassword.value,
+        curso: inputCourse.value,
+        centro: inputCenter.value,
     }
 
     fetch("http://localhost:3000/v1/tecnicos/register", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(newTecnico)
-}) //  Aqui acaba el fetch
-    .then(response => response.json())
-    .then(response => {
-        console.log("LA INSERCIÓN UTILIZANDO POST SE HA REALIZADO CORRECTAMENTE")
-        console.log(response)
-        console.log("---------------------------------------------------------")
-    })
-    .catch(error => console.log(error));
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newTecnico)
+        }) //  Aqui acaba el fetch
+        .then(response => response.json())
+        .then(response => {
+            console.log("LA INSERCIÓN UTILIZANDO POST SE HA REALIZADO CORRECTAMENTE")
+            console.log(response)
+            console.log("---------------------------------------------------------")
+        })
+        .catch(error => console.log(error));
 }
-
-
-
-btnsub.addEventListener("click", processForm)
